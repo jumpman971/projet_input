d9fa54fca8535bf5ebfc101aae4e0e7fe725dd4d
Taka <you@example.com>
2018-10-24 21:58:44 +0200

    ajout de plateforme mobile (manque une vitesse de déplacement) et fix paramètre du wall jump

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+    [SerializeField]
+    public Vector3 Velocity;
+
+    public Vector3 LastLocation;
+
+    public Vector3 firstMovingBound;
+    public Vector3 secondMovingBound;
+
+    public float speed;
+
+    public float timeBeforeNextMove;
+    private float startTime;
+    private int step;
+    private bool isMoving = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+        step = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving && LastLocation == transform.position) {
+            startTime = Time.time;
+            isMoving = false;
+        }
+
+        if (!isMoving && startTime + timeBeforeNextMove < Time.time) {
+            if (step == 0) {
+                Velocity = firstMovingBound;
+                ++step;
+            } else {
+                Velocity = secondMovingBound;
+                step = 0;
+            }
+            isMoving = true;
+        }
+
+        transform.position = transform.position + Velocity;
+
+        LastLocation = transform.position;
+
+        Velocity /= 1.2f;
+        //Velocity /= 5f;
+        //Velocity += Gravity;
+    }
+}
